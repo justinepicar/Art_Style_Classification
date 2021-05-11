16e0e8ee2b4ec8f2bfff0fa86ee5a5c5b3b350b4
Justine Picar <ohhmannjustine@gmail.com>
2021-05-11 07:23:35 -0700

    updated files and functions

--- createimages.py
+++ createimages.py
@@ -1,13 +1,11 @@
 
 import os
+import shutil
 import numpy as np
 
 def clear_old_images(path, df):
     num_skipped = 0
-    #listdir = os.listdir(imagepath)
-    #number_files = len(listdir)
 
-    #if number_files >= df.sample_quantity.sum(): #num_folders >= len(artlabels.genre.unique())
     for i in range(len(df)):
         imgpath = os.path.join(path, df.genre[i])
         if os.path.exists(imgpath):
@@ -18,9 +16,10 @@ def clear_old_images(path, df):
                     num_skipped += 1
                     #Delete image
                     os.remove(fpath)
-    #print("Deleted %d images" % num_skipped) #from % imagepath
 
-    return num_skipped
+    print(f'Deleted {num_skipped} images') #from % imagepath
+
+    return None
 
 
 def get_random_imgs(oldpath, newpath, df, qty):
@@ -31,31 +30,27 @@ def get_random_imgs(oldpath, newpath, df, qty):
     returns dataframe of files sampled with labels'''
 
     sample_list = []  # pd.DataFrame(columns=['label', 'filename'])
-    num_skipped = 0
     copied = 0
 
-    if os.path.exists(newpath):
-        num_skipped = clear_old_images(newpath, df)
-
     for i in range(len(df)):
-        rawpath = os.path.join(oldpath, df.name[i])
+        rawpath = os.path.join(oldpath, df.name[i]) #not the issue
+        imgpath = os.path.join(newpath, df.genre[i])  # not the issue
         files = [f for f in os.listdir(rawpath)]  # if os.path.isdir(f)]
-        random_files = np.random.choice(files, int(round(qty * df['% of paintings'][i])))
-        imgpath = os.path.join(newpath, df.genre[i])
-        # check if folder exists; if it exists, clear any old residual images before new samples
-        if os.path.exists(imgpath):
-            pass
-        #    num_skipped+=clear_old_images(imgpath)
+        sample = int(round(qty * df['% of paintings'][i])) #not the issue
+        if len(os.listdir(rawpath)) >= sample:
+            random_files = np.random.choice(files, sample)
         else:
+            random_files = os.listdir(rawpath)
+        # check if folder exists; if it exists, clear any old residual images before new samples
+        if not os.path.exists(newpath):
             create_new_folder(imgpath)
-        # create a dataframe to return label and filename
+        #create a dataframe to return label and filename
         for copy in random_files:
             path_to_copy = os.path.join(rawpath, copy)
             shutil.copy(path_to_copy, imgpath)
             sample_list.append([df.genre[i], copy])  # append(path_to_copy) #df.genre[i] and copy
             copied += 1
 
-    print("Deleted %d images" % num_skipped)
-    print("Generated %d new images" % copied)
+    print(f'Generated {copied} new images')
 
     return sample_list
