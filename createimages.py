bbd343246ac7a00d595c2843a9a0238b2b33a392
Justine Picar <ohhmannjustine@gmail.com>
2021-06-09 22:57:50 -0700

    added get_image_info function from function.py

--- createimages.py
+++ createimages.py
@@ -63,3 +63,27 @@ def get_images(oldpath, newpath, df, genres, frac):
             copied += 1
 
     print(f'Generated {copied} new images')
+
+def get_image_info(directory):
+    ''' this function returns labels, filename, and
+        the full filepath of each image
+        param: directory
+        return: list of labels, filename, full_path'''
+
+    labels = []
+    full_path = []
+    filename = []
+
+    for folder_name in os.listdir(directory):
+        folder_path = os.path.join(directory, folder_name)  # images/name
+        for fname in os.listdir(folder_path):
+            fpath = os.path.join(folder_path, fname)  # images/name/pic.jpg
+            if fpath is not None:
+                filename.append(fname)
+                fullpath.append(fpath)
+                labels.append(folder_name)
+
+    partialpath = get_directory(directory)
+    classfolders = get_class_folder(directory)
+
+    return [labels, filename, full_path]
--- function.py
+++ function.py
@@ -1,34 +1,14 @@
 import os
 
 def create_new_folder(path):
+    '''
+
+    :param path:
+    :return:
+    '''
     if not os.path.exists(path):
         os.makedirs(path)
         print(path + ' created')
     else:
         print(path + ' path already exists')
     return None
-
-
-def get_image_info(directory):
-    ''' this function returns labels, filename, and
-        the full filepath of each image
-        param: directory
-        return: list of labels, filename, full_path'''
-
-    labels = []
-    full_path = []
-    filename = []
-
-    for folder_name in os.listdir(directory):
-        folder_path = os.path.join(directory, folder_name)  # images/name
-        for fname in os.listdir(folder_path):
-            fpath = os.path.join(folder_path, fname)  # images/name/pic.jpg
-            if fpath is not None:
-                filename.append(fname)
-                fullpath.append(fpath)
-                labels.append(folder_name)
-
-    partialpath = get_directory(directory)
-    classfolders = get_class_folder(directory)
-
-    return [labels, filename, full_path]
