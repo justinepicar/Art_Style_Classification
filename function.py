8e792bfbce477507430d139bc39d08b66d018235
Justine Picar <ohhmannjustine@gmail.com>
2021-06-09 22:46:27 -0700

    consolidated get_directory, get_image_info, and get_class_folder to one function

--- function.py
+++ function.py
@@ -8,34 +8,6 @@ def create_new_folder(path):
         print(path + ' path already exists')
     return None
 
-def get_class_folder(folder):
-    ''' this function returns
-        a list of all class folder names
-        param: folder
-        return: folder_list '''
-    folder_list = []
-    for f in os.listdir(folder):
-        folder_list.append(f)
-
-    return folder_list
-
-
-def get_directory(directory):
-    ''' this function creates the directory path
-        and returns a partial path to the images
-        param: directory
-        return: partialpath'''
-
-    partialpath = []
-    classfolders = get_class_folder(directory)
-
-    for folder in classfolders:
-        path = os.path.join(directory, folder)
-        if path is not None:
-            partialpath.append(path)
-
-    return partialpath
-
 
 def get_image_info(directory):
     ''' this function returns labels, filename, and
@@ -43,27 +15,20 @@ def get_image_info(directory):
         param: directory
         return: list of labels, filename, full_path'''
 
-    # for name in os.listdir(directory):
-    # if not name.endswith('.jpg'):
-    #    continue
-    # else:
-
-    partialpath = get_directory(directory)
-    classfolders = get_class_folder(directory)
-
     labels = []
     full_path = []
     filename = []
 
-    i = 0
+    for folder_name in os.listdir(directory):
+        folder_path = os.path.join(directory, folder_name)  # images/name
+        for fname in os.listdir(folder_path):
+            fpath = os.path.join(folder_path, fname)  # images/name/pic.jpg
+            if fpath is not None:
+                filename.append(fname)
+                fullpath.append(fpath)
+                labels.append(folder_name)
 
-    for path in partialpath:
-        for name in os.listdir(path):
-            img = os.path.join(path, name)
-            if img is not None:
-                full_path.append(img)
-                filename.append(name)
-                labels.append(classfolders[i])
-        i += 1
+    partialpath = get_directory(directory)
+    classfolders = get_class_folder(directory)
 
     return [labels, filename, full_path]
